4c2e235eb608064b83dcc7fc3c8f7675208542ae
Marcia Allen <mallen57@email.phoenix.edu>
2016-11-06 17:32:37 -0500

    ASCIIArt
    ASCIIArt Initial Commit

---
+++ ASCIIArt/ASCIIArt.cpp
@@ -0,0 +1,37 @@
+// ASCIIArt.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+int main(int argc, char *argv[]) {
+
+	// Draw triangle
+	cout << "   *   " << endl;
+	cout << "  ***  " << endl;
+	cout << " ***** " << endl;
+	cout << "*******" << endl;
+	cout << "  ***  " << endl;
+	cout << "\n";
+	cout << "\n";
+	cout << "\n";
+
+	// Draw Cat Under the Tree
+	cout << " /\\   /\\" << endl;
+	cout << "   o o   " << endl;
+	cout << "  =   = " << endl;
+	cout << "   ---   " << endl;
+	cout << "\n";
+	cout << "\n";
+	cout << "\n";
+
+	//Draw Semi-Truck Under the Cat
+	cout << "__________________ ||" << endl;
+	cout << "|^^^^^^^^^^^^^^^^\\||____" << endl;
+	cout << "|   Super Cool   |||''|'\\__,_|_" << endl;
+	cout << "| ______________ |||__|___|__|)|" << endl;
+	cout << "(@)@)''''''''''**|(@) (@)**|(@)" << endl;
+
+	return 0;
+}
